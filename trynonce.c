ba12016d5f0772fa8381978e4e04a45c0eb4e287
Jay Sullivan <jay@petio.org>
2010-04-08 10:10:11 -0400

    fix format

--- trynonce.c
+++ trynonce.c
@@ -3,9 +3,7 @@
 int main(int argc, char *argv[])
 {
 	char nonce[12];
-	buffer_putsflush(buffer_1,"{");
 	nonce_next("mynonce",nonce);
 	nonce_show(nonce);
-	buffer_putsflush(buffer_1,"}\n");
 	return 0;
 }
