c1bae529bb5caafbecbf592a28a7fdf362a045fa
Jay Sullivan <jay@petio.org>
2010-09-04 19:58:08 -0400

    remove .a files

--- nonce.h
+++ nonce.h
@@ -4,5 +4,6 @@
 #define RAND_BYTES 4
 void nonce_create(const char *filepath);
 void nonce_next(const char *filepath, char *nonce);
+void nonce_separate(char *nonce, int nserv,int serv);
 void nonce_show(const char *nonce);
 #endif
